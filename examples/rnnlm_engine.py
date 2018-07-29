528d010b2f7322a2938926472bbfebdaf79df4ac
Steffen Remus <steremus@gmail.com>
2018-07-29 22:19:34 +0200

    revert adam

--- examples/rnnlm_engine.py
+++ examples/rnnlm_engine.py
@@ -144,7 +144,6 @@ try:
     output_flat = output.view(-1, ntokens)
     targets_flat = y_batch.contiguous().view(-1)  
     loss = criterion(output_flat, targets_flat)
-#    print(loss.item())
     return loss, output_flat
     
   def on_start(state):
@@ -162,8 +161,8 @@ try:
     if state['train']:
       # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
       torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
-#      for p in model.parameters():
-#        p.data.add_(-lr, p.grad.data)
+      for p in model.parameters():
+        p.data.add_(-lr, p.grad.data)
       state['total_train_loss'] += state['loss'].item()
     state['total_test_loss'] += state['loss'].item()
     
@@ -217,14 +216,12 @@ try:
   engine.hooks['on_end_epoch'] = on_end_epoch
   
   dummyoptimizer = torch.optim.Adam([torch.autograd.Variable(torch.Tensor(1), requires_grad = True)])
-#  optimizer = torch.optim.SGD(model.parameters(), lr = 20)
-  optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
   
   ###############################################################################
   # run training
   ###############################################################################
   
-  final_state = engine.train(process, train_loader, maxepoch=args.epochs, optimizer=optimizer)
+  final_state = engine.train(process, train_loader, maxepoch=args.epochs, optimizer=dummyoptimizer)
   
   # Load the best saved model.
   with open(args.save, 'rb') as f:
