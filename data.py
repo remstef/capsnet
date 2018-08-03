f0d2f83bd330342183c915e24f05fed86c1f5b75
Steffen Remus <steremus@gmail.com>
2018-08-03 15:41:00 +0200

    cleanup

--- data.py
+++ data.py
@@ -24,12 +24,6 @@ from utils import Index
 from sklearn.datasets import fetch_20newsgroups
 import pickle
 
-#emb = RandomEmbedding(300) # random vectors
-#emb = TextEmbedding('./GoogleNews-vectors-negative300.txt').load(nlines = 1000)
-#emb = TextEmbedding('./glove.840B.300d.txt').load(nlines = 1000, skipheader = False, normalize = True, nlines = 1000)
-#emb = FastTextEmbedding('./wiki.en.bin').load()
-
-
 '''
  Sequences in a single 1d tensor
 '''
@@ -229,7 +223,7 @@ class SpamDataset(torch.utils.data.Dataset):
 
     def load_data(self):
       # do some preprocessing if preprocessed file does not exist
-      if not os.path.isfile('SMSSpamCollection_normalized'):
+      if not os.path.isfile('SMSSpamCollection_normalized.pkl'):
         #import spacy; nlp=spacy.load('en')
         print('Applying spacy.')
         import en_core_web_sm
@@ -294,41 +288,41 @@ class SpamDataset(torch.utils.data.Dataset):
 
       return ( torch.tensor(mat), int_label, oh_label, index )
 
-    def get_sample(self, index):
-        return self.samples[index]
+#    def get_sample(self, index):
+#        return self.samples[index]
 
 
-    def load_samples_vectors(self):
-        #Average Word_count train: 94
-        #Average Word_count test: 88.2
-        # av_len = 0
-        # for msg in self.samples:
-        #     av_len+= len(msg)
-        # print(av_len / len(self.samples))
-        #print(len(self.samples))
-
-        mat = np.zeros( (len(self.samples),self.maxlength, 300) )
-        # filter unknowns
-        for j, msg in enumerate(self.samples):
-          for i, token in enumerate(msg):
-            word = self.samples[j][i]
-            if not emb.containsWord(word):
-              self.samples[j].remove(word)
-            if emb.containsWord(word) and not emb.containsWord(token):
-              print(word, token, " DAFUQ")
-
-        for j, msg in enumerate(self.samples):
-          #print(msg)
-          for i, token in enumerate(msg):
-            if i >= self.maxlength:
-              break
-            if not emb.containsWord(token):
-              #print(token)
-              continue
-            v = emb.getVector(token)
-            #print(v)
-            mat[j, i, :] = v
-        return mat
+#    def load_samples_vectors(self):
+#        #Average Word_count train: 94
+#        #Average Word_count test: 88.2
+#        # av_len = 0
+#        # for msg in self.samples:
+#        #     av_len+= len(msg)
+#        # print(av_len / len(self.samples))
+#        #print(len(self.samples))
+#
+#        mat = np.zeros( (len(self.samples),self.maxlength, 300) )
+#        # filter unknowns
+#        for j, msg in enumerate(self.samples):
+#          for i, token in enumerate(msg):
+#            word = self.samples[j][i]
+#            if not emb.containsWord(word):
+#              self.samples[j].remove(word)
+#            if emb.containsWord(word) and not emb.containsWord(token):
+#              print(word, token, " DAFUQ")
+#
+#        for j, msg in enumerate(self.samples):
+#          #print(msg)
+#          for i, token in enumerate(msg):
+#            if i >= self.maxlength:
+#              break
+#            if not emb.containsWord(token):
+#              #print(token)
+#              continue
+#            v = emb.getVector(token)
+#            #print(v)
+#            mat[j, i, :] = v
+#        return mat
 
 
 class NewsGroupDataset(torch.utils.data.Dataset):
@@ -406,39 +400,38 @@ class NewsGroupDataset(torch.utils.data.Dataset):
     def get_sample(self, index):
         return self.samples[index]
 
-    def load_samples_vectors(self):
-        #Average Word_count train: 94
-        #Average Word_count test: 88.2
-        # av_len = 0
-        # for msg in self.samples:
-        #     av_len+= len(msg)
-        # print(av_len / len(self.samples))
-        #print(len(self.samples))
-        mat = np.zeros( (len(self.samples),self.maxlength, 300) )
+#    def load_samples_vectors(self):
+#        #Average Word_count train: 94
+#        #Average Word_count test: 88.2
+#        # av_len = 0
+#        # for msg in self.samples:
+#        #     av_len+= len(msg)
+#        # print(av_len / len(self.samples))
+#        #print(len(self.samples))
+#        mat = np.zeros( (len(self.samples),self.maxlength, 300) )
+#
+#        # filter unknowns
+#        for j, msg in enumerate(self.samples):
+#          for i, token in enumerate(msg):
+#            word = self.samples[j][i]
+#            if not emb.containsWord(word):
+#              self.samples[j].remove(word)
+#            if emb.containsWord(word) and not emb.containsWord(token):
+#              print(word, token, " DAFUQ")
+#
+#        for j, msg in enumerate(self.samples):
+#          #print(msg)
+#          for i, token in enumerate(msg):
+#            if i >= self.maxlength:
+#              break
+#            if not emb.containsWord(token):
+#              #print(token)
+#              continue
+#            v = emb.getVector(token)
+#            #print(v)
+#            mat[j, i, :] = v
+#        return mat
 
-        # filter unknowns
-        for j, msg in enumerate(self.samples):
-          for i, token in enumerate(msg):
-            word = self.samples[j][i]
-            if not emb.containsWord(word):
-              self.samples[j].remove(word)
-            if emb.containsWord(word) and not emb.containsWord(token):
-              print(word, token, " DAFUQ")
-
-        for j, msg in enumerate(self.samples):
-          #print(msg)
-          for i, token in enumerate(msg):
-            if i >= self.maxlength:
-              break
-            if not emb.containsWord(token):
-              #print(token)
-              continue
-            v = emb.getVector(token)
-            #print(v)
-            mat[j, i, :] = v
-        return mat
-
-        return mat
 
 class DummyDataset(torch.utils.data.Dataset):
     def __init__(self, num_classes, dimensions, num_samples):
@@ -463,84 +456,4 @@ class DummyDataset(torch.utils.data.Dataset):
         label_oh[cycle] = 1
         return sample, label, label_oh, index
 
-    def get_data_dimensions(self):
-        return self.dimensions
-      
-class SemEval2008(torch.utils.data.Dataset):
-
-    '''
-      maxlength = size of padding
-    '''
-    def __init__(self, maxlength = 30):
-      super(SemEval2008, self)
-      self.load_data()
-
-    def load_data(self):
-      # do some preprocessing if preprocessed file does not exist
-      if not os.path.isfile('semeval-2008-sample-processed.txt'):
-        #import spacy; nlp=spacy.load('en')
-        print('Applying spacy.')
-        import en_core_web_sm
-        nlp = en_core_web_sm.load()
-
-        samples = pandas.read_csv(
-            'semeval-2008-sample.txt',
-            sep=' ',
-            quoting=csv.QUOTE_ALL,
-            names=['id', 'originalsentence'],
-            encoding='UTF-8')
-        samples['original_offset_e1'] = samples.originalsentence.apply(lambda s: (s.find('<e1>'), s.find('</e1>')))
-        samples['original_offset_e2'] = samples.originalsentence.apply(lambda s: (s.find('<e2>'), s.find('</e2>')))
-        samples['e1'] = samples.apply(lambda row: row.originalsentence[row.original_offset_e1[0] + 4:row.original_offset_e1[1]], axis=1)
-        samples['e2'] = samples.apply(lambda row: row.originalsentence[row.original_offset_e2[0] + 4:row.original_offset_e2[1]], axis=1)
-        samples['l'] = samples.apply(lambda row: row.originalsentence[:row.original_offset_e1[0]], axis=1)
-        samples['r'] = samples.apply(lambda row: row.originalsentence[row.original_offset_e2[1]+5:], axis=1)
-        samples['m'] = samples.apply(lambda row: row.originalsentence[row.original_offset_e1[1]+5:row.original_offset_e2[0]], axis=1)
-        samples['sentence'] = samples.apply(lambda row: row.l+row.e1+row.m+row.e2+row.r, axis=1)
-        samples['offset_e1'] = samples.apply(lambda row: (len(row.l), len(row.l) + len(row.e1)), axis=1)
-        samples['offset_e2'] = samples.apply(lambda row: (len(row.l) + len(row.e1) + len(row.m), len(row.l) + len(row.e1) + len(row.m) + len(row.e2)), axis=1)
-        samples['spacy'] = samples.sentence.apply(lambda s: nlp(s))
-        samples.to_pickle('semeval-2008-sample-processed.pickle')
-        del samples
-
-      # load processed messages
-      self.samples = pandas.read_pickle('semeval-2008-sample-processed.pickle')
-
-      self.classes = list(set(samples.labels))
-      self.classes.sort()
-      self.classes = dict([(l,i) for (i,l) in enumerate(self.classes)])
-      
-      return True
-    
-
-    def __len__(self):
-      return len(self.samples)
-
-
-    '''
-      @return (item as matrix of token vectors, label, index) instead of the normal (item, label)
-    '''
-    def __getitem__(self, index):
-      msg = self.samples[index]
-      int_label = self.classes[self.labels[index]]   #Convert label to integer. Is this slow?
-      #mat = np.zeros( (self.maxlength, self.ftmodel.numpy_normalized_vectors.shape[1]) )
-      mat = np.zeros( (self.maxlength, 300) )   #Hard coded for the wikipedia data that has dim=300
-
-      for i, token in enumerate(msg):
-
-        if i >= self.maxlength:
-          break
-        if not emb.containsWord(token):
-          continue
-        v = emb.getVector(token)
-
-        mat[i,:] = v
-        oh_label = self.oh_classes.transform([[self.labels[index]]])
-        oh_label = oh_label[0]
-        #print(oh_label)
-
-      return ( torch.tensor(mat), int_label, oh_label, index )
-
-    def get_sample(self, index):
-        return self.samples[index]
 
