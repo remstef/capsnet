f99bc4d277d36a2fb40378f6962aea631f8b961d
Steffen Remus <steremus@gmail.com>
2018-08-08 19:59:26 +0200

    rex1

--- utils.py
+++ utils.py
@@ -249,7 +249,7 @@ def createWrappedOptimizerClass(optimizer_clazz):
 def makeOneHot(X, ntoken):
   # X = batch_size x seq
   batch_size, seqlen = X.size()
-  X_one_hot = torch.zeros(batch_size, seqlen , ntoken)
+  X_one_hot = torch.zeros(batch_size, seqlen , ntoken).todevice(X.device)
   X_one_hot.scatter_(2, X.unsqueeze(2), 1.)
   return X_one_hot
 
